1a2518f83f2a43fb967d5f8a60e266616e17233a
herve guerre <herveguerre@hotmail.fr>
2023-06-13 11:54:37 +0200

    hyer

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+                <div>Hyer<span>R</span></div>
+                
+                <div className='burger' ></div>
+
+        </div>
+    )
+}
