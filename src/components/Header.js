16aa2da223fe22bf24fb4359726108e70c0ad3bc
herve guerre <herveguerre@hotmail.fr>
2023-06-13 20:39:29 +0200

    maj

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import './Style.css'
+import '../app/globals.css'
 
 export default function Header() {
     return (
-        <div>
-                <div>Hyer<span>R</span></div>
-                
-                <div className='burger' ></div>
+        <div className='header'>
+                <div className='logo'>Hyer<span>R</span></div>
+                <div className='nav'>
+                    <li>Solution</li>
+                    <li>A propos de nous</li>
+                    <li>Contact</li>
+                    <h2> Avion <br/>personnalisée</h2>
+                </div>
 
+                <div className='burger' >
+                        <div class="line"></div>
+                        <div class="line"></div>
+                        <div class="line"></div>
+                </div>
         </div>
     )
 }
