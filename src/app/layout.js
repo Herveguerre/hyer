90965d5590788eb92df2681e76f56796b13d6c67
herve guerre <herveguerre@hotmail.fr>
2023-06-15 20:57:25 +0200

    maj

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="fr">
       <Header/>
       <body className={inter.className}>{children}</body>
+      <Footer/>
     </html>
   )
 }
