640354ed22d934b034c5a1dd3ed531acf57b2e25
herve guerre <herveguerre@hotmail.fr>
2023-06-15 09:22:55 +0200

    maj

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <div className='fg'>
+            <div className='rs'></div>
+            <div className='copyright'></div>
+        </div>
+        <div className='fd'></div>
+    </div>
+  )
+}
