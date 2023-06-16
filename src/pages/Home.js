fd3a21eafa09886398a59e1ceba166af7df9fd97
herve guerre <herveguerre@hotmail.fr>
2023-06-16 17:15:13 +0200

    maj

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,8 @@ import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import React from 'react'
 import './Style.css'
-import Section1 from '@/components/home/section1'
-import Section2 from '@/components/home/section2'
+import Section1 from '@/components/element_home/section1'
+import Section2 from '@/components/element_home/section2'
 
 export default function Home() {
   return (
