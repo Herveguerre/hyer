b0897ddcaab0ae6f59e89fd085a0052ab1e615a9
herve guerre <herveguerre@hotmail.fr>
2023-06-18 20:18:49 +0200

    maj

---
+++ src/components/element_about/SectionA1.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SectionA1() {
+  return (
+    <div>SectionA1</div>
+  )
+}
---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import SectionA1 from '@/components/element_about/SectionA1'
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>About
+
+        <Header/>
+        <SectionA1/>
+        <Footer/>
+    </div>
+  )
+}
