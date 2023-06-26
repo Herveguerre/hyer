017e7b2d752d2995b88682b1e3159d43a2f72f07
herve guerre <herveguerre@hotmail.fr>
2023-06-26 09:29:57 +0200

    about

--- src/components/aboutPage/AvisClients.js
+++ src/components/aboutPage/AvisClients.js
@@ -2,6 +2,30 @@ import React from 'react'
 
 export default function AvisClients() {
   return (
-    <div>AvisClients</div>
+    <div>
+      <div  className='container flex wrap'><hr/>
+        <div>
+          <p>
+          Ce que disent nos clients
+          </p>
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <p>Lors de mes vols privés, mon Flight Manager Valérie était là pour m'aider à répondre à toutes mes questions ou préoccupations. 
+        <br/>~ Jesper
+      </p>
+
+      <p>Je vole avec Hyer en tant que membre ELEVATE depuis un certain temps maintenant, après avoir été un client Netjets insatisfait. 
+        <br/> Nicolas
+      </p>
+
+      <p>Nous avons eu un mariage exceptionnel à Capri.Hyer nous a aidés avec leur charte et a conduit nos 50 invités en toute sécurité à l'aéroport de Naples. 
+        <br/>Marque
+      </p>
+      <hr/>
+    </div>
   )
 }
